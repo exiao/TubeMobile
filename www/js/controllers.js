153d11d6a01fb14914a44f66bee86cd4837d85d8
System Administrator <root@MBA62-128.local>
2014-08-14 15:00:23 -0700

    dashboard is now campaigns list page

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,14 +1,15 @@
 angular.module('starter.controllers', [])
 
-.controller('DashCtrl', function($scope) {
+.controller('campaigns_controller', function($scope, Campaigns) {
+    $scope.campaigns = Campaigns.all();
 })
 
 .controller('FriendsCtrl', function($scope, Friends) {
-  $scope.friends = Friends.all();
+    $scope.friends = Friends.all();
 })
 
 .controller('FriendDetailCtrl', function($scope, $stateParams, Friends) {
-  $scope.friend = Friends.get($stateParams.friendId);
+    $scope.friend = Friends.get($stateParams.friendId);
 })
 
 .controller('AccountCtrl', function($scope) {
--- www/js/services.js
+++ www/js/services.js
@@ -1,26 +1,37 @@
 angular.module('starter.services', [])
 
-/**
- * A simple example service that returns some data.
- */
-.factory('Friends', function() {
-  // Might use a resource here that returns a JSON array
+.factory('Campaigns', function() {
+    var campaigns = [
+        { id: 0, name: 'Campaign 0' },
+        { id: 1, name: 'Campaign 1' },
+        { id: 2, name: 'Campaign 2' },
+        { id: 3, name: 'Campaign 3' }
+    ];
 
-  // Some fake testing data
-  var friends = [
-    { id: 0, name: 'Scruff McGruff' },
-    { id: 1, name: 'G.I. Joe' },
-    { id: 2, name: 'Miss Frizzle' },
-    { id: 3, name: 'Ash Ketchum' }
-  ];
-
-  return {
-    all: function() {
-      return friends;
-    },
-    get: function(friendId) {
-      // Simple index lookup
-      return friends[friendId];
+    return {
+        all: function() {
+            return campaigns;
+        },
+        get: function(id) {
+            return campaigns[id];
+        }
     }
-  }
+})
+
+.factory('Friends', function() {
+    var friends = [
+        { id: 0, name: 'Scruff McGruff' },
+        { id: 1, name: 'G.I. Joe' },
+        { id: 2, name: 'Miss Frizzle' },
+        { id: 3, name: 'Ash Ketchum' }
+    ];
+
+    return {
+        all: function() {
+          return friends;
+        },
+        get: function(friendId) {
+          return friends[friendId];
+        }
+    }
 });
